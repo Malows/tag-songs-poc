162d92f9d17860b5e9ad206f1ddbe8d15b65932e
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2022-02-01 18:15:50 -0300

    Tag badge components

---
+++ src/lib/components/TagBadge.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { goto } from "$app/navigation";
+    import TagBadgeBase from "./TagBadgeBase.svelte";
+
+    export let tag;
+
+    function click () {
+        goto(`/tags/${tag.slug}`);
+    }
+</script>
+
+
+<TagBadgeBase on:click={click} outline={false}>
+    {tag.name}
+</TagBadgeBase>>
---
+++ src/lib/components/TagBadgeBase.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let outline: Boolean;
+</script>
+
+<span
+    class="px-4 py-2 text-base rounded-full"
+    class:bg-orange-600={!outline}
+    class:border-orange-600={outline}
+    class:text-white={!outline}
+    class:text-orange-600={outline}
+    on:click
+    >
+    <slot />
+</span>
