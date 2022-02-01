d13dbdd15dfec1187b8cb5eea2052ae07784031a
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2022-02-01 18:17:43 -0300

    Song display prototype

---
+++ src/lib/components/Song.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let song = null;
+</script>
+
+<div class="flex bg-gray-200 rounded-lg p-4 m-2">
+    <div class="w-64 bg-gray-400 rounded-lg"></div>
+    <div class="flex flex-col items-start ml-4">
+        <h4 class="text-xl font-semibold">{song.name}</h4>
+        <p class="text-sm">Some text about the thing that goes over a few lines.</p>
+
+        <div class="flex justify-evenly">
+            {#if song.spotify_url}
+                <a class="p-2 leading-none rounded font-medium mt-3 bg-gray-400 text-xs uppercase" href={song.spotify_url}>Click Here</a>
+            {/if}
+
+            {#if song.spotify_url}
+                <a class="p-2 leading-none rounded font-medium mt-3 bg-gray-400 text-xs uppercase" href={song.youtube_url}>Click Here</a>
+            {/if}
+        </div>
+    </div>
+</div>
